b59d39ab087f040a96eb7827afa129e829294d3a
Randy La Rosa Alvarez <rra@wedoo.tech>
2024-04-05 05:26:17 -0400

    as

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -9,13 +9,8 @@ setuptools.setup(
     version=version,
     install_requires=[
         'odoo-addon-account_financial_report>=15.0dev,<15.1dev',
-        'odoo-addon-account_move_line_report_xls>=15.0dev,<15.1dev',
-        'odoo-addon-account_purchase_stock_report_non_billed>=15.0dev,<15.1dev',
-        'odoo-addon-account_sale_stock_report_non_billed>=15.0dev,<15.1dev',
         'odoo-addon-account_tax_balance>=15.0dev,<15.1dev',
         'odoo-addon-mis_builder_cash_flow>=15.0dev,<15.1dev',
-        'odoo-addon-mis_template_financial_report>=15.0dev,<15.1dev',
-        'odoo-addon-partner_statement>=15.0dev,<15.1dev',
     ],
     classifiers=[
         'Programming Language :: Python',
